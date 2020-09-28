6af58a461449f7861c8d151e080dcc4069596200
Dhevendran.Kulandaivelu <k.dhevendran@gmail.com>
2020-09-28 15:27:55 +0530

    The latested as on 28th Sept

--- go-grpc-with-db/db/client/cmd/create.go
+++ go-grpc-with-db/db/client/cmd/create.go
@@ -28,14 +28,9 @@ import (
 
 // createCmd represents the create command
 var createCmd = &cobra.Command{
-	Use:   "create",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Use:   "create <First Name> <Last Name> <ID>",
+	Short: "Create a DB entry for the given detals",
+	Long:  `Create a DB entry for the given detals.`,
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println("Create called")
 		postUser(args)
--- go-grpc-with-db/db/client/cmd/get.go
+++ go-grpc-with-db/db/client/cmd/get.go
@@ -27,14 +27,9 @@ import (
 
 // getCmd represents the get command
 var getCmd = &cobra.Command{
-	Use:   "get",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Use:   "get <ID>",
+	Short: "Gets the details of the given ID.",
+	Long:  `Gets the details of the given ID.`,
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println("get called")
 		getUser(args)
